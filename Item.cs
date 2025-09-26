ae15b6a8b35edabdb023be366dc684f69920dace
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2025-09-26 12:10:10 +0200

    Create Item and User objects
    Created according to UML specification

---
+++ Item.cs
@@ -0,0 +1,13 @@
+namespace TradeSys;
+
+class Item
+{
+    private string Name;
+    private string Description;
+
+    public Item(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,13 @@
+namespace TradeSys;
+
+class User
+{
+    private string Username;
+    private string password;
+
+    public User(string username, string password)
+    {
+        Username = username;
+        password = password;
+    }
+}

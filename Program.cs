44ed70467f988ced6b063be8ba3d2caca3a68473
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2025-09-26 12:00:18 +0200

    Add Screen switch handling, and rudimentary login method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
 ﻿using TradeSys;
 
+int CurrentScreen = (int) Screen.Login;
 
-Console.WriteLine(TradeStatus.Accepted);
+bool is_running = true;
+
+Console.Clear();
+while (is_running)
+{
+    switch (CurrentScreen)
+    {
+        case (int)Screen.Login:
+            return;
+    }
+
+    Console.WriteLine("Press Any Key to Continue");
+    string Input = Console.ReadLine().ToLower();
+
+    is_running = false;
+}
+
+void LoginScreen()
+{
+    Console.WriteLine("Log in to your user account:");
+    Console.WriteLine("Username:");
+    string User = Console.ReadLine();
+    Console.WriteLine("Password:");
+    string Pass = Console.ReadLine();
+}
